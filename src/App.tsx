25ec6e715e54c5856fcf5eebd074338fde4f1034
ReallyRatt <141839304+reallyratt@users.noreply.github.com>
2026-03-14 15:30:38 +0700

    fix: Remove unnecessary start parameter from YouTube embed
    The `start` parameter was causing the YouTube video to always begin at a specific timestamp, irrespective of the desired playback. Removing it ensures the video plays from its intended start.

--- src/App.tsx
+++ src/App.tsx
@@ -358,7 +358,7 @@ export default function App() {
           <iframe
             width="100"
             height="100"
-            src="https://www.youtube-nocookie.com/embed/ENVxylS0ew4?start=2254&autoplay=1&controls=0&loop=1&playlist=ENVxylS0ew4"
+            src="https://www.youtube-nocookie.com/embed/ENVxylS0ew4?autoplay=1&controls=0&loop=1&playlist=ENVxylS0ew4"
             allow="autoplay"
           />
         </div>
